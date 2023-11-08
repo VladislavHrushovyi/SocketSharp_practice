6696f605d9388f2514c8c25dd07a179f74c9b168
vladg <vlad.grush.99@gmail.com>
2023-11-08 12:32:06 +0200

    init project

---
+++ SimpleSocket/MySocket.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class MySocket
+{
+    private readonly TcpListener _tcpListener;
+
+    public MySocket(string ip, int port)
+    {
+        IPAddress localAddress = IPAddress.Parse(ip);
+        _tcpListener = new TcpListener(localAddress, port);
+    }
+
+    public void ServerStart()
+    {
+        _tcpListener.Start();
+        try
+        {
+            while (true)
+            {
+                TcpClient client = _tcpListener.AcceptTcpClient();
+
+                NetworkStream stream = client.GetStream();
+                byte[] data = new byte[256];
+                int bytesRead = stream.Read(data, 0, data.Length);
+                string message = Encoding.ASCII.GetString(data, 0, data.Length);
+                Console.WriteLine("Користувач: " + message);
+                byte[] reply = Encoding.ASCII.GetBytes("Повідомлення отримано");
+                stream.Write(reply, 0, reply.Length);
+
+                client.Close();
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+        finally
+        {
+            _tcpListener.Stop();
+        }
+    }
+}
---
+++ SimpleSocket/Program.cs
@@ -0,0 +1,3 @@
+﻿
+var server = new MySocket("127.0.0.1", 10000);
+server.ServerStart();
