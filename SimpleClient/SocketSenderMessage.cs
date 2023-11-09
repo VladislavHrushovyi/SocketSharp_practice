7d78ab46f64ac1b19d34c19984c8e0c6a942b68c
vladg <vlad.grush.99@gmail.com>
2023-11-09 14:18:45 +0200

    upgrade messaging logic

--- SimpleClient/SocketSenderMessage.cs
+++ SimpleClient/SocketSenderMessage.cs
@@ -1,5 +1,4 @@
-﻿using System.Net;
-using System.Net.Sockets;
+﻿using System.Net.Sockets;
 using System.Text;
 
 namespace SimpleClient;
@@ -7,31 +6,44 @@ namespace SimpleClient;
 public class SocketSenderMessage
 {
     private readonly TcpClient _tcpClient;
+    private readonly NetworkStream _stream;
     public SocketSenderMessage(string ip, int port)
     {
         _tcpClient = new TcpClient();
         _tcpClient.Connect(ip, port);
+        _stream = _tcpClient.GetStream();
     }
 
     public void SendMessage(string text)
     {
-        try
-        {
-            using NetworkStream stream = _tcpClient.GetStream();
-            byte[] data = Encoding.ASCII.GetBytes(text);
-            stream.Write(data, 0, data.Length);
+        var thread = new Thread((tcpClient) => ReceiveData((TcpClient)tcpClient));
+        thread.Start(_tcpClient);
 
-
-            data = new byte[256];
-            string responseText = String.Empty;
-            int readBytes = stream.Read(data, 0, data.Length);
-            responseText = Encoding.ASCII.GetString(data, 0, readBytes);
-            Console.WriteLine("Відповідь: " + responseText);
-        }
-        catch (Exception e)
+        string s = String.Empty;
+        while (!string.IsNullOrEmpty(s = Console.ReadLine()))
         {
-            Console.WriteLine(e);
-            throw;
+            byte[] buffer = Encoding.ASCII.GetBytes(s);
+            _stream.Write(buffer, 0, buffer.Length);
         }
+        
+        _tcpClient.Client.Shutdown(SocketShutdown.Send);
+        thread.Join();
+        _stream.Close();
+        _tcpClient.Close();
+        Console.WriteLine("User has been disconected");
+        Console.Read();
+    }
+    private Task ReceiveData(TcpClient tcpC)
+    {
+        var stream = tcpC.GetStream();
+        byte[] buffer = new byte[1024];
+        int byteCount;
+        
+        while ((byteCount = stream.Read(buffer, 0, buffer.Length)) > 0)
+        {
+            Console.WriteLine(Encoding.ASCII.GetString(buffer, 0, byteCount));
+        }
+        
+        return Task.CompletedTask;
     }
 }
--- SimpleSocket/MySocket.cs
+++ SimpleSocket/MySocket.cs
@@ -4,32 +4,29 @@ using System.Text;
 
 public class MySocket
 {
+    private readonly IDictionary<int, TcpClient> _clients = new Dictionary<int, TcpClient>();
+    private readonly object _lockObject = new();
     private readonly TcpListener _tcpListener;
 
     public MySocket(string ip, int port)
     {
         IPAddress localAddress = IPAddress.Parse(ip);
         _tcpListener = new TcpListener(localAddress, port);
+        _tcpListener.Start();
     }
 
     public void ServerStart()
     {
-        _tcpListener.Start();
         try
         {
             while (true)
             {
                 TcpClient client = _tcpListener.AcceptTcpClient();
 
-                NetworkStream stream = client.GetStream();
-                byte[] data = new byte[256];
-                int bytesRead = stream.Read(data, 0, data.Length);
-                string message = Encoding.ASCII.GetString(data, 0, bytesRead);
-                Console.WriteLine("Користувач: " + message);
-                byte[] reply = Encoding.ASCII.GetBytes("Повідомлення отримано");
-                stream.Write(reply, 0, reply.Length);
+                lock(_lockObject) _clients.Add(_clients.Count + 1, client);
+                Console.WriteLine($"Connected: {client.Client.AddressFamily}");
 
-                client.Close();
+                Task.Run(() => HandleClient(_clients.Count));
             }
         }
         catch (Exception e)
@@ -40,6 +37,55 @@ public class MySocket
         finally
         {
             _tcpListener.Stop();
+        }
+    }
+
+    private Task HandleClient(int clientsCount)
+    {
+        int id = clientsCount;
+        TcpClient client;
+
+        lock (_lockObject) client = _clients[id];
+
+        while (true)
+        {
+            NetworkStream stream = client.GetStream();
+
+            byte[] buffer = new byte[1024];
+            if (!stream.CanRead)
+            {
+                continue;
+            }
+            int byte_count = stream.Read(buffer, 0, buffer.Length);
+            
+            if (byte_count == 0)
+            {
+                break;
+            }
+
+            string data = Encoding.ASCII.GetString(buffer, 0, byte_count);
+            BroadcastData(data);
+            Console.WriteLine(data);
+        }
+
+        lock (_lockObject) _clients.Remove(id);
+        client.Client.Shutdown(SocketShutdown.Both);
+        client.Close();
+        
+        return Task.CompletedTask;
+    }
+
+    private void BroadcastData(string data)
+    {
+        byte[] buffer = Encoding.ASCII.GetBytes(data + Environment.NewLine);
+
+        lock (_lockObject)
+        {
+            foreach (var client in _clients.Values)
+            {
+                NetworkStream stream = client.GetStream();
+                stream.Write(buffer, 0, buffer.Length);
+            }
         }
     }
 }
