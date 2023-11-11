86fa3906f26a8480c1c8c2ab8abd8037d0302685
vladg <vlad.grush.99@gmail.com>
2023-11-11 16:19:19 +0200

    init react project

---
+++ online-drawer/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <div>
+          МАЛЮВАЛКА
+          <canvas className=''>
+
+          </canvas>
+        </div>
+    </>
+  )
+}
+
+export default App
