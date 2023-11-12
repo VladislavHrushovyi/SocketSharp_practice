68282595f168da61cbdd8b2354eceb02359f7793
vladg <vlad.grush.99@gmail.com>
2023-11-12 17:39:26 +0200

    minimal drawing features

--- online-drawer/src/components/DrawField.tsx
+++ online-drawer/src/components/DrawField.tsx
@@ -1,69 +1,86 @@
-import {useEffect, useRef, useState } from "react";
-import { Button } from "react-bootstrap";
+import { useEffect, useRef, useState } from "react";
+import { Button, Col, Row } from "react-bootstrap";
 
 export const DrawField = () => {
-    const canvas = useRef<HTMLCanvasElement>();
-    const [pos, setPos] = useState({x:0,y:0});
+    const canvas = useRef<HTMLCanvasElement | null>(null);
+    const [pos, setPos] = useState({ x: 0, y: 0 });
 
     const setPosition = (e: React.MouseEvent<HTMLCanvasElement>) => {
-        setPos({
-            x: e.clientX,
-            y: e.clientY
-        });
+        if (e.buttons !== 1) {
+            return;
+        }
+
+        const canvasRect = canvas.current?.getBoundingClientRect();
+        if (canvasRect) {
+            setPos({
+                x: e.clientX - canvasRect.left,
+                y: e.clientY - canvasRect.top
+            });
+        }
     };
 
     const resize = () => {
         const ctx = canvas.current?.getContext("2d");
-        ctx!.canvas.width = window.innerWidth;
-        ctx!.canvas.height = window.innerHeight;
+        ctx!.canvas.width = 900;
+        ctx!.canvas.height = 500;
     };
 
     const draw = (e: React.MouseEvent<HTMLCanvasElement>) => {
         if (e.buttons !== 1) {
             return;
         }
+
         const ctx = canvas.current?.getContext("2d");
-        ctx!.beginPath();
-        ctx!.lineWidth = 10;
-        ctx!.lineCap = "butt";
-        ctx!.strokeStyle = "green";
-        ctx!.moveTo(pos.x, pos.y);
-        setPosition(e);
-        ctx!.lineTo(pos.x, pos.y);
-        ctx!.stroke();
+        if (ctx) {
+            ctx!.beginPath();
+            ctx!.lineWidth = 5;
+            ctx!.lineCap = "round";
+            ctx!.strokeStyle = "red";
+            ctx!.moveTo(pos.x, pos.y);
+            setPosition(e);
+            ctx!.lineTo(pos.x, pos.y);
+            ctx!.stroke();
+        }
     };
 
     const clear = () => {
         const ctx = canvas.current?.getContext("2d");
-        ctx?.clearRect(0,0, canvas.current?.width!, canvas.current?.height!);
+        ctx?.beginPath();
+        ctx?.clearRect(0, 0, canvas.current?.width!, canvas.current?.height!);
+    };
+
+    const endDrawing = () => {
+        const base64Canvas = canvas.current?.toDataURL().split(';base64,')[1];
+        console.log(base64Canvas)
     }
 
     useEffect(() => {
-        window.addEventListener("resize", resize);
-        return () => window.removeEventListener("resize", resize);
+        resize();
     }, []);
 
     return (
-        <div>
-            <style>
-                {`
-          #canvas {
-            border: 1px solid black;
-          }
-        `}
-            </style>
-            <canvas
-                ref={canvas}
-                onMouseMove={draw}
-                onMouseDown={setPosition}
-                onMouseEnter={setPosition}
-                id="canvas"
-            ></canvas>
-            <Button
-                onClick={clear}
-            >
-                CLEAR
-            </Button>
-        </div>
+        <Row>
+            <Col>
+                <canvas
+                    ref={canvas}
+                    onMouseMove={(e) => draw(e)}
+                    onMouseDown={(e) => setPosition(e)}
+                    onMouseEnter={(e) => setPosition(e)}
+                    onMouseUp={endDrawing}
+                ></canvas>
+            </Col>
+            <Col>
+                <Button
+                    onClick={clear}
+                >
+                    CLEAR
+                </Button>
+                <Button
+                    onClick={resize}
+                >
+                    ReSIZE
+                </Button>
+            </Col>
+        </Row>
     );
 }
