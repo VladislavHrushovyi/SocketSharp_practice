76dea82bba5e94e7ade0bf0a953d7412869ad284
vladg <vlad.grush.99@gmail.com>
2023-11-19 13:11:03 +0200

    delete logging

--- SignalRServer/Hubs/ImageHub.cs
+++ SignalRServer/Hubs/ImageHub.cs
@@ -4,12 +4,8 @@ namespace SignalRServer.Hubs;
 
 public class ImageHub : Hub
 {
-    private readonly ImageResource _imageResource = new();
     public async Task NewMessage(string data)
     {
-        Console.WriteLine("RECEIVE REQ");
-        Console.WriteLine(string.Join("",data.Skip(data.Length - 20).Take(20)));
-        _imageResource.ImageBase64 = data;
-        await Clients.All.SendAsync("ImageReceived", _imageResource.ImageBase64);
+        await Clients.All.SendAsync("ImageReceived", data);
     }
 }
