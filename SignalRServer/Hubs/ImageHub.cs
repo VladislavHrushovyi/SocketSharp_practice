48c0114a43a4ac745b0deb9a3765ac6ac9b07e27
vladg <vlad.grush.99@gmail.com>
2023-11-21 15:45:30 +0200

    tracking connection or disconnection users

--- SignalRServer/Hubs/ImageHub.cs
+++ SignalRServer/Hubs/ImageHub.cs
@@ -4,8 +4,25 @@ namespace SignalRServer.Hubs;
 
 public class ImageHub : Hub
 {
+    private static int _countConnection = 0;
     public async Task NewMessage(string data)
     {
         await Clients.All.SendAsync("ImageReceived", data);
+    }
+
+    public override Task OnConnectedAsync()
+    {
+        _countConnection++;
+        Console.WriteLine($"Users: {_countConnection}");
+        this.Clients.All.SendAsync("AmountConnection",_countConnection);
+        return base.OnConnectedAsync();
+    }
+
+    public override Task OnDisconnectedAsync(Exception? exception)
+    {
+        _countConnection--;
+        Console.WriteLine($"Users: {_countConnection}");
+        this.Clients.All.SendAsync("AmountConnection",_countConnection);
+        return base.OnDisconnectedAsync(exception);
     }
 }
