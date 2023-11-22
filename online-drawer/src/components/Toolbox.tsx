df60c62f7a2382be991ca36c632a6f21604e5c58
vladg <vlad.grush.99@gmail.com>
2023-11-22 12:10:33 +0200

    add new buttons

--- online-drawer/src/components/Toolbox.tsx
+++ online-drawer/src/components/Toolbox.tsx
@@ -1,9 +1,9 @@
-import { Row } from "react-bootstrap"
+import { Button, Row } from "react-bootstrap"
 import { LineWidthRange } from "./LineWidthRange"
 import { ColorPicker } from "./ColorPicker"
 import { MyButton } from "./MyButton"
 import { ToolboxUtils } from "../hooks/useToolbox"
-
+import { EraserFill, Eyedropper, PencilFill } from "react-bootstrap-icons"
 interface ToolboxProps {
     toolboxUtils: ToolboxUtils
 }
@@ -13,6 +13,17 @@ export const Toolbox = ({toolboxUtils}: ToolboxProps) => {
         <>
             <Row>
                 <ColorPicker handleColor={toolboxUtils.changeColor} color={toolboxUtils.color} />
+            </Row>
+            <Row className="py-4 flex justify-center space-x-4" >
+                <Button className="border rounded-lg p-2 hover:bg-green-300"> 
+                    <PencilFill size={20}/> 
+                </Button >
+                <Button className="border rounded-lg p-2 hover:bg-green-300">
+                    <EraserFill size={20}/>
+                </Button>
+                <Button className="border rounded-lg p-2 hover:bg-green-300">
+                    <Eyedropper size={20}/>
+                </Button>
             </Row>
             <Row>
                 <LineWidthRange width={toolboxUtils.lineWidth} handleLineWidth={toolboxUtils.changeLineWidth} />
